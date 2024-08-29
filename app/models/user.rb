fe3ada4a9bdfd2442665d1b775ebd3833ffc4ffd
ZiadAmrEmbabi <zamr@madfoat.com>
2024-08-29 14:44:27 +0300

    adding password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase}
   has_many :articles
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -15,4 +16,5 @@ class User < ApplicationRecord
             length: { minimum: 3, maximum: 100 },
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
